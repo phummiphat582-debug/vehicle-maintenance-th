025c7dfe0fdab27d52be926a872377e41ff7e55d
phummiphat582-debug <phummiphat582@gmail.com>
2026-01-15 15:29:40 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,11 @@
-const CACHE_NAME = "sc-maintenance-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
-
-self.addEventListener("install", e => {
+const CACHE="sc-maintenance-v1";
+self.addEventListener("install",e=>{
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE).then(c=>c.addAll([
+      "./","./index.html","./manifest.json"
+    ]))
   );
 });
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
